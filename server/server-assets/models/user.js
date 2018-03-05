2f3a7ee2428f9f3be7bdc3f9e01d987155fc336f
Caleb Adrian <calebadrian1847@gmail.com>
2018-03-05 12:05:27 -0700

    initial setup for users

---
+++ server/server-assets/models/user.js
@@ -0,0 +1,23 @@
+var mongoose = require("mongoose");
+var bcrypt = require("bcryptjs");
+var Schema = mongoose.Schema;
+var ObjectId = mongoose.SchemaTypes.ObjectId;
+var schemaName = "User";
+const SALT_FACTOR = 13;
+
+var schema = new Schema({
+  name: { type: String},
+  email: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: true, minlength: 6 },
+  created: { type: Number, default: Date.now(), required: true },
+});
+
+schema.statics.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(SALT_FACTOR));
+};
+
+schema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model(schemaName, schema);
