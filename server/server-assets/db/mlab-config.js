8167845cb19ddb23fac6ccb7aa6b9f31ba5881ac
Nic <nicjorgi@gmail.com>
2018-03-05 12:05:18 -0700

    m-lab

---
+++ server/server-assets/db/mlab-config.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose");
+var connectionString = "mongodb://kanban:kanban@ds012578.mlab.com:12578/kanban";
+
+var connection = mongoose.connection;
+mongoose.connect(connectionString);
+
+connection.on("error", err => {
+    console.error("mlab Error: ", err);
+});
+
+connection.once("open", () => {
+    console.log("connected to database");
+});
