86a36a527770ab52590ecb04b194c8473d88346e
Caleb Adrian <calebadrian1847@gmail.com>
2018-03-05 14:53:49 -0700

    router changes and auth logout

--- public/src/App.vue
+++ public/src/App.vue
@@ -6,6 +6,7 @@
 
 <script>
   import Home from './components/Home'
+  import router from './router'
 
 export default {
   name: 'App',
--- public/src/components/Home.vue
+++ public/src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-
+    <h3>Hey</h3>
   </div>
 </template>
 
