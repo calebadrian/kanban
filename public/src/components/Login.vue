491d5f87b22724b328a3e4706e9d97a601dfe820
Nic <nicjorgi@gmail.com>
2018-03-05 14:30:26 -0700

    Login Component

--- public/src/components/Login.vue
+++ public/src/components/Login.vue
@@ -1,21 +1,45 @@
 <template>
     <div class="login">
+        <form v-if="showLogin == true" @submit.prevent="login">
+            <input type="text" v-model="user.email" placeholder="email">
+            <input type="password" v-model="user.password" placeholder="password">
+            <button type="submit">Submit</button>
+        </form>
+
+        <form v-if="showLogin == false" @submit.prevent="createUser">
+            <input type="text" v-model="user.name" placeholder="username">
+            <input type="text" v-model="user.email" placeholder="email">
+            <input type="password" v-model="user.password" placeholder="password">
+            <input type="password" v-model="user.password" placeholder="password">
+            <button type="submit" @click"showLogin = true">Submit</button>
+        </form>
+
+        <button @click="showLogin = false">Create User</button>
+        <button @click="showLogin = true">Login</button>
 
     </div>
 </template>
 
 <script>
-    import User from './User'
-
     export default {
         name: 'Login',
         data() {
             return {
-
+                showlogin : true,
+                user: {
+                    name: '',
+                    email: '',
+                    password: ''
+                }
             }
         },
-        components: {
-            User
+        methods: {
+            login() {
+                this.$store.dispatch('login', this.user)
+            },
+            createUser() {
+                this.$store.dispatch('createUser', this.user)
+            }
         }
     }
 
