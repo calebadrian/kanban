b43fe388deb375bb80e83a062789e1d20d9ead41
Nic <nicjorgi@gmail.com>
2018-03-06 12:11:45 -0700

    Login Styling

--- public/src/components/Login.vue
+++ public/src/components/Login.vue
@@ -1,20 +1,24 @@
 <template>
     <div class="login">
-        <form v-if="showLogin == true" @submit.prevent="login()">
-            <input type="text" v-model="user.email" placeholder="email">
-            <input type="password" v-model="user.password" placeholder="password">
-            <button type="submit">Submit</button>
+            <img src="../assets/Omeganize_logo2.jpg" class="logo">
+
+        <form v-if="showLogin == true" @submit.prevent="login()" class="form-group">
+            <input type="text" v-model="user.email" placeholder="email" class="form-control">
+            <input type="password" v-model="user.password" placeholder="password" class="form-control">
+            <button type="submit" class="btn btn-info">Submit</button>
         </form>
 
-        <form v-if="showLogin == false" @submit.prevent="createUser()">
-            <input type="text" v-model="newUser.name" placeholder="username">
-            <input type="text" v-model="newUser.email" placeholder="email">
-            <input type="password" v-model="newUser.password" placeholder="password">
-            <button type="submit" @click="showLogin = true">Submit</button>
+        <form v-if="showLogin == false" @submit.prevent="createUser()" class="form-group">
+            <input type="text" v-model="newUser.name" placeholder="username" class="form-control">
+            <input type="text" v-model="newUser.email" placeholder="email" class="form-control">
+            <input type="password" v-model="newUser.password" placeholder="password" class="form-control">
+            <button type="submit" @click="showLogin = true" class="btn btn-info">Submit</button>
         </form>
 
-        <button @click="showLogin = false">Create User</button>
-        <button @click="showLogin = true">Login</button>
+        <div class="bg-1">
+            <button @click="showLogin = false" class="btn btn-link">Create User</button>
+            <button @click="showLogin = true" class="btn btn-link">Login</button>
+        </div>
 
     </div>
 </template>
@@ -50,4 +54,44 @@
 
 
 <style scoped>
+    .login {
+        background-image: url('../assets/bg-1.jpg');
+        background-size: cover;
+        height: 100vh
+    }
+
+    .logo {
+        opacity: .7;
+        border-radius: 30px;
+        margin: 15px;
+
+    }
+
+    .form-group {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center
+    }
+
+    .form-control {
+        width: 50%;
+        margin: 5px
+    }
+
+    .bg-1 {
+        background-color: rgba(218, 216, 216, 0.596);
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around
+    }
+
+    .btn-link {
+        color: indigo;
+        font-weight: 700
+    }
+
+    .btn-info {
+        width: 25%
+    }
 </style>
