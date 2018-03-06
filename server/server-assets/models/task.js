13fb2f99f60bde7135d77af9536b54628c28a005
Caleb Adrian <calebadrian1847@gmail.com>
2018-03-06 15:58:41 -0700

    added hooks for remove functions

--- server/server-assets/models/task.js
+++ server/server-assets/models/task.js
@@ -2,6 +2,7 @@ var mongoose = require("mongoose")
 var Schema = mongoose.Schema
 var ObjectId = mongoose.SchemaTypes.ObjectId
 var SchemaName = "Task"
+var Comments = require("../models/comment")
 
 var priorities = ["high", "medium", "low"]
 
@@ -11,6 +12,11 @@ var schema = new Schema({
     boardId: {type: ObjectId, ref: 'Board'},    
     description: {type: String},
     priority: {type: String, enum: priorities, default: priorities[1]}
+})
+
+schema.pre('findByIdAndRemove', function(next) {
+    Comments.remove({taskId: this._id}).exec()
+    next()
 })
 
 module.exports = mongoose.model(SchemaName, schema)
